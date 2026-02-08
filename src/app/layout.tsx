ceb114bba787aeda45119f4fd976a890a419b084
haitani <haitani@tam-tam.co.jp>
2026-02-08 18:26:04 +0900

    scaffdog、app/layout.tsx、generate-component.md を追加 #46

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "@/styles/index.scss";
+
+export const metadata: Metadata = {
+  title: "CURRENT GRAVITY（カレントグラビティ） - 価値志向プロファイリングモデル",
+  description:
+    "あなたやチームのプロファイリングモデル。責任の引き受け方、関係の持ち方の地図。",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>
+        <div id="root">
+          <main>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
